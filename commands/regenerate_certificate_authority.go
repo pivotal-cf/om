c584f1f4d2ab7153a26af25e53693ed6f218265f
Kalai Wei <kawei@pivotal.io>
2017-10-04 14:25:18 -0700

    Regenerate fakes using newer counterfeiter
    [#142194165]
    
    Signed-off-by: Rizwan Reza <rreza@pivotal.io>

--- commands/regenerate_certificate_authority.go
+++ commands/regenerate_certificate_authority.go
@@ -1,15 +1,35 @@
 package commands
 
+import "github.com/pivotal-cf/jhanda/commands"
+
 type RegenerateCertificateAuthority struct {
-	service certificateAuthoritiesRegenerator
+	service certificateAuthorityRegenerator
 	logger  logger
 }
 
-//go:generate counterfeiter -o ./fakes/certificate_authority_regenerator.go --fake-name CertificateAuthorityGenerator . certificateAuthorityRegenerator
-type certificateAuthoritiesRegenerator interface {
+//go:generate counterfeiter -o ./fakes/certificate_authority_regenerator.go --fake-name CertificateAuthorityRegenerator . certificateAuthorityRegenerator
+type certificateAuthorityRegenerator interface {
 	Regenerate() error
 }
 
-func NewRegenerateCertificateAuthority(service certificateAuthoritiesRegenerator, logger logger) RegenerateCertificateAuthority {
+func NewRegenerateCertificateAuthority(service certificateAuthorityRegenerator, logger logger) RegenerateCertificateAuthority {
 	return RegenerateCertificateAuthority{service: service, logger: logger}
+}
+
+func (r RegenerateCertificateAuthority) Execute(_ []string) error {
+	err := r.service.Regenerate()
+	if err != nil {
+		return err
+	}
+
+	r.logger.Printf("Certificate authority regenerated.\n")
+
+	return nil
+}
+
+func (r RegenerateCertificateAuthority) Usage() commands.Usage {
+	return commands.Usage{
+		Description:      "This authenticated command regenerates certificate authority on the Ops Manager",
+		ShortDescription: "regenerates a certificate authority on the Opsman",
+	}
 }
--- commands/regenerate_certificate_authority_test.go
+++ commands/regenerate_certificate_authority_test.go
@@ -1,8 +1,11 @@
 package commands_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	jhandacommands "github.com/pivotal-cf/jhanda/commands"
 	"github.com/pivotal-cf/om/commands"
 	"github.com/pivotal-cf/om/commands/fakes"
 )
@@ -15,14 +18,30 @@ var _ = Describe("RegenerateCertificateAuthority", func() {
 	)
 
 	BeforeEach(func() {
+		fakeCertificateAuthorityService = &fakes.CertificateAuthorityRegenerator{}
+		fakeLogger = &fakes.Logger{}
 		command = commands.NewRegenerateCertificateAuthority(fakeCertificateAuthorityService, fakeLogger)
 	})
 
 	Describe("Execute", func() {
 		It("makes a request to the Opsman to regenerate an inactive certificate authority", func() {
-			err := command.Execute()
+			err := command.Execute([]string{})
 			Expect(err).NotTo(HaveOccurred())
-			Expect(fakeCertificateAuthorityService.GenerateCallCount()).To(Equal(1))
+			Expect(fakeCertificateAuthorityService.RegenerateCallCount()).To(Equal(1))
+
+			Expect(fakeLogger.PrintfCallCount()).To(Equal(1))
+			format, content := fakeLogger.PrintfArgsForCall(0)
+			Expect(fmt.Sprintf(format, content...)).To(Equal("Certificate authority regenerated.\n"))
+		})
+	})
+
+	Describe("Usage", func() {
+		It("returns usage info", func() {
+			usage := command.Usage()
+			Expect(usage).To(Equal(jhandacommands.Usage{
+				Description:      "This authenticated command regenerates certificate authority on the Ops Manager",
+				ShortDescription: "regenerates a certificate authority on the Opsman",
+			}))
 		})
 	})
 })
