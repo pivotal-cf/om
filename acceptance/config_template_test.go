633897b9d65478ca5417df56430a01a91bed4606
Kevin Kelani <kkelani@pivotal.io>
2018-04-02 14:18:11 -0700

    unfocus acceptance test
    * fix help acceptance tests
    
    [#155568214]
    
    Signed-off-by: Rizwan Reza <rreza@pivotal.io>

--- acceptance/config_template_test.go
+++ acceptance/config_template_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-var _ = FDescribe("config-template command", func() {
+var _ = Describe("config-template command", func() {
 	var (
 		product *os.File
 	)
