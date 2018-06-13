5c9641ae043f3c3897d5609e9da194a07bb4d350
Huan Wang <huwang@pivotal.io>
2018-06-13 16:54:16 -0600

    add sha256digest func
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

---
+++ digest/init_test.go
@@ -0,0 +1,14 @@
+package digest_test
+
+import (
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+
+    "testing"
+)
+
+
+func TestCommands(t *testing.T) {
+    RegisterFailHandler(Fail)
+    RunSpecs(t, "digest")
+}
