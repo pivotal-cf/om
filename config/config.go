8a9d9d80dd87ac7d9b998df1c8c7f9a9cc2eabe3
Chris Hendrix <chendrix@pivotal.io>
2018-08-20 10:19:41 -0700

    Keep product-properties key when empty
    - ops-manifest depends on there being a product-properties key
    - planitest parses and rewrites a config that merges a base config
      and any run-time updates
    
    [#159866851] Upgrade planitest in our tiles
    
    Signed-off-by: Michelle He <mhe@pivotal.io>

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 type ProductConfiguration struct {
-	ProductProperties        map[string]interface{}  `yaml:"product-properties,omitempty"`
+	ProductProperties        map[string]interface{}  `yaml:"product-properties"`
 	NetworkProperties        map[string]interface{}  `yaml:"network-properties,omitempty"`
 	ResourceConfigProperties map[string]interface{}  `yaml:"resource-config,omitempty"`
 	ErrandConfigs            map[string]ErrandConfig `yaml:"errand-config,omitempty"`
