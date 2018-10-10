6ec680fe5809673781e20aff7890f8085a8e67fb
JT Archie <jarchie@pivotal.io>
2018-10-10 15:33:20 -0600

    task that builds om binaries for all GOOS
    Signed-off-by: Huan Wang <huwang@pivotal.io>

---
+++ ci/tasks/build/task.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+
+function main() {
+  local cwd
+  cwd="${1}"
+
+  local version
+  version="$(cat om-version/version)"
+
+  export GOPATH="${cwd}/go"
+  pushd "${GOPATH}/src/github.com/pivotal-cf/om" > /dev/null
+    for OS in darwin linux windows; do
+      local name
+      name="om-${OS}"
+
+      if [[ "${OS}" == "windows" ]]; then
+        name="${name}.exe"
+      fi
+
+      CGO_ENABLED=0 \
+      GOOS=${OS} \
+      GOARCH=amd64 \
+        go build \
+          -ldflags "-X main.version=${version}" \
+          -o "${cwd}/binaries/${name}" \
+          main.go
+    done
+  popd > /dev/null
+}
+
+main "${PWD}"
