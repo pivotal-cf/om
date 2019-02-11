0244317e4570f44faa9164f0445f2c8c84307a3b
Jesse Alford <jalford@pivotal.io>
2019-02-11 10:43:34 -0700

    Add pending test for a failure case we just found
    Signed-off-by: John McBride <jmcbride@pivotal.io>

--- commands/download_product_test.go
+++ commands/download_product_test.go
@@ -572,6 +572,19 @@ output-directory: %s
 			})
 		})
 
+		PContext("when neither product-version nor product-version-regex are set", func() {
+			It("fails with an error saying that the user must provide one or the other", func() {
+				err = command.Execute([]string{
+					"--pivnet-api-token", "token",
+					"--pivnet-file-glob", "*.pivotal",
+					"--pivnet-product-slug", "elastic-runtime",
+					"--output-directory", tempDir,
+				})
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("no version information provided; please provide either --product-version or --product-version-regex"))
+			})
+		})
+
 		Context("when the release specified is not available", func() {
 			BeforeEach(func() {
 				fakePivnetDownloader.ReleaseForVersionReturns(pivnet.Release{}, fmt.Errorf("some-error"))
