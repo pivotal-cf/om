2fc3f73ec724dd6b73c5da1a991c790cf3032518
JT Archie <jarchie@pivotal.io>
2019-02-20 18:47:53 -0700

    have acceptance test do suite setup once

--- acceptance/download_product_test.go
+++ acceptance/download_product_test.go
@@ -15,59 +15,31 @@ import (
 var _ = Describe("download-product command", func() {
 	When("downloading from s3", func() {
 		var (
-			minio      *gexec.Session
-			targetName string
-			port       int
+			bucketName string
 		)
-
-		runCommand := func(args ...string) {
-			fmt.Fprintf(GinkgoWriter, "cmd: %s", args)
-			command := exec.Command(args[0], args[1:]...)
-			configure, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-			Expect(err).ToNot(HaveOccurred())
-			Eventually(configure, "10s").Should(gexec.Exit(0))
-		}
 
 		BeforeEach(func() {
 			_, err := exec.LookPath("minio")
 			if err != nil {
 				Skip("minio not installed")
 			}
-			fmt.Println("\nRunning minio v2 signing test")
 			_, err = exec.LookPath("mc")
 			if err != nil {
 				Skip("mc not installed")
 			}
 
-			// start minio
-			dataDir, err := ioutil.TempDir("", "")
-			Expect(err).ToNot(HaveOccurred())
-			port = 9000 + config.GinkgoConfig.ParallelNode
-			command := exec.Command("minio", "server", "--config-dir", dataDir, "--address", fmt.Sprintf(":%d", port), dataDir)
-			command.Env = []string{
-				"MINIO_ACCESS_KEY=minio",
-				"MINIO_SECRET_KEY=password",
-				"MINIO_BROWSER=off",
-				"TERM=xterm-256color",
-			}
-			minio, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
-			Expect(err).NotTo(HaveOccurred())
-
-			Eventually(minio.Out).Should(gbytes.Say("Endpoint:"))
-
 			// upload artifact to it
-			targetName = fmt.Sprintf("testing-%d", port)
-			runCommand("mc", "--debug", "config", "host", "add", targetName, fmt.Sprintf("http://127.0.0.1:%d", port), "minio", "password")
-			runCommand("mc", "--debug", "mb", targetName+"/bucket")
+			bucketName = fmt.Sprintf("bucket-%d", config.GinkgoConfig.ParallelNode)
+			runCommand("mc", "--debug", "mb", "testing/"+bucketName)
 		})
 
 		AfterEach(func() {
-			minio.Kill()
+			runCommand("mc", "--debug", "rm", "--force", "--recursive", "testing/"+bucketName)
 		})
 
 		When("specifying the version of the AWS signature", func() {
 			It("supports v2 signing", func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product.yml")
 				tmpDir, err := ioutil.TempDir("", "")
 				Expect(err).ToNot(HaveOccurred())
 				command := exec.Command(pathToMain, "download-product",
@@ -77,11 +49,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 					"--s3-enable-v2-signing", "true",
 				)
 
@@ -92,7 +64,7 @@ var _ = Describe("download-product command", func() {
 			})
 
 			It("supports v4 signing", func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product.yml")
 				tmpDir, err := ioutil.TempDir("", "")
 				Expect(err).ToNot(HaveOccurred())
 				command := exec.Command(pathToMain, "download-product",
@@ -102,11 +74,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -128,11 +100,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -144,10 +116,10 @@ var _ = Describe("download-product command", func() {
 
 		When("a file with a prefix for the desired slug/version is not found", func() {
 			BeforeEach(func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/example-product-1.10.1_product.yml")
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/still-useless.yml")
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,2.22.3]product-456.yml")
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,2.22.2]product-123.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/example-product-1.10.1_product.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/still-useless.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,2.22.3]product-456.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,2.22.2]product-123.yml")
 			})
 
 			It("raises an error that no files with a prefixed name matching the slug and version are available", func() {
@@ -160,11 +132,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -176,7 +148,7 @@ var _ = Describe("download-product command", func() {
 
 		When("one prefixed file matches the product slug and version", func() {
 			BeforeEach(func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product.yml")
 			})
 
 			It("outputs the file and downloaded file metadata", func() {
@@ -189,11 +161,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -212,8 +184,8 @@ var _ = Describe("download-product command", func() {
 
 		When("more than one prefixed file matches the product slug and version", func() {
 			BeforeEach(func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product-456.yml")
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product-123.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product-456.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product-123.yml")
 			})
 
 			It("raises an error that too many files match the glob", func() {
@@ -226,11 +198,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version", "1.10.1",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
@@ -242,8 +214,8 @@ var _ = Describe("download-product command", func() {
 
 		When("using product-regex to find the latest version", func() {
 			BeforeEach(func() {
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.1]product-123.yml")
-				runCommand("mc", "--debug", "cp", "fixtures/product.yml", targetName+"/bucket/[example-product,1.10.2]product-456.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.1]product-123.yml")
+				runCommand("mc", "--debug", "cp", "fixtures/product.yml", "testing/"+bucketName+"/[example-product,1.10.2]product-456.yml")
 			})
 
 			It("raises an error that too many files match the glob", func() {
@@ -256,11 +228,11 @@ var _ = Describe("download-product command", func() {
 					"--product-version-regex", "1.*",
 					"--output-directory", tmpDir,
 					"--blobstore", "s3",
-					"--s3-bucket", "bucket",
+					"--s3-bucket", bucketName,
 					"--s3-access-key-id", "minio",
 					"--s3-secret-access-key", "password",
 					"--s3-region-name", "unknown",
-					"--s3-endpoint", fmt.Sprintf("http://127.0.0.1:%d", port),
+					"--s3-endpoint", "http://127.0.0.1:9001",
 				)
 
 				session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
--- acceptance/init_test.go
+++ acceptance/init_test.go
@@ -1,7 +1,11 @@
 package acceptance
 
 import (
+	"fmt"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
+	"io/ioutil"
+	"os/exec"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,19 +15,53 @@ import (
 
 var (
 	pathToMain string
+	minio      *gexec.Session
 )
 
-var _ = BeforeSuite(func() {
+var _ = SynchronizedBeforeSuite(func() []byte {
 	var err error
 	pathToMain, err = gexec.Build("github.com/pivotal-cf/om", "-ldflags", "-X main.applySleepDurationString=1ms")
 	Expect(err).NotTo(HaveOccurred())
+
+	minioPath, _ := exec.LookPath("minio")
+	if minioPath != "" {
+		dataDir, err := ioutil.TempDir("", "")
+		Expect(err).ToNot(HaveOccurred())
+		command := exec.Command("minio", "server", "--config-dir", dataDir, "--address", ":9001", dataDir)
+		command.Env = []string{
+			"MINIO_ACCESS_KEY=minio",
+			"MINIO_SECRET_KEY=password",
+			"MINIO_BROWSER=off",
+			"TERM=xterm-256color",
+		}
+		minio, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(minio.Out).Should(gbytes.Say("Endpoint:"))
+		runCommand("mc", "--debug", "config", "host", "add", "testing", "http://127.0.0.1:9001", "minio", "password")
+	}
+	return []byte(pathToMain)
+}, func(data []byte) {
+	pathToMain = string(data)
 })
 
-var _ = AfterSuite(func() {
+var _ = SynchronizedAfterSuite(func() {
+}, func() {
+	if minio != nil {
+		minio.Kill()
+	}
 	gexec.CleanupBuildArtifacts()
 })
 
 func TestAcceptance(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "acceptance")
+}
+
+func runCommand(args ...string) {
+	fmt.Fprintf(GinkgoWriter, "cmd: %s\n", args)
+	command := exec.Command(args[0], args[1:]...)
+	configure, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).ToNot(HaveOccurred())
+	Eventually(configure, "10s").Should(gexec.Exit(0))
 }
