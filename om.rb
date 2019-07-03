a2fc0727d60f39ceb9316fb4a7cb5426873c3f56
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-03 17:03:02 -0600

    Brew formula update for om version 2.0.1

--- om.rb
+++ om.rb
@@ -2,14 +2,14 @@
 class Om < Formula
   desc ""
   homepage ""
-  version "2.0.0"
+  version "2.0.1"
 
   if OS.mac?
-    url "https://github.com/pivotal-cf/om/releases/download/2.0.0/om-darwin-2.0.0.tar.gz"
-    sha256 "48455a4a7da6ab91047a30dfc6105080cab3e491300ff21961319d601043cfb7"
+    url "https://github.com/pivotal-cf/om/releases/download/2.0.1/om-darwin-2.0.1.tar.gz"
+    sha256 "578235fed7ce64a16d2883f98bc91dbc63aafde1c1bf59664c6fb5edc3814b47"
   elsif OS.linux?
-    url "https://github.com/pivotal-cf/om/releases/download/2.0.0/om-linux-2.0.0.tar.gz"
-    sha256 "e3432b9361886c71a9e03391728de3eeba3c416ec7048426dba98ab81ba66551"
+    url "https://github.com/pivotal-cf/om/releases/download/2.0.1/om-linux-2.0.1.tar.gz"
+    sha256 "c8ef55ce07e97ff9eadbd8e3e1c7909407378733c19ff39348b4b1e1bc3a65e5"
   end
 
   def install
