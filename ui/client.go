82aa250b2f1749588576471eb073d63a9b6f8861
JT Archie <jarchie@pivotal.io>
2019-07-11 17:09:57 -0600

    remove ui package
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- ui/client.go
+++
@@ -1,9 +0,0 @@
-package ui
-
-import "net/http"
-
-//go:generate counterfeiter -o ./fakes/httpclient.go --fake-name HttpClient . httpClient
-
-type httpClient interface {
-	Do(*http.Request) (*http.Response, error)
-}
