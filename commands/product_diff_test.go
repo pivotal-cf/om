59e028cab757f83f4d7b5c6235008d7e6cc73a4b
Jesse Alford <jalford@pivotal.io>
2019-12-06 14:48:14 -0700

    Extract test setup and add runtime configs to fixture
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- commands/product_diff_test.go
+++ commands/product_diff_test.go
@@ -26,26 +26,41 @@ var _ = Describe("ProductDiff", func() {
 		service = &fakes.ProductDiffService{}
 	})
 
-	When("a valid product is provided", func(){
+	When("a valid product is provided", func() {
+		BeforeEach(func() {
+			service.ProductDiffReturns(
+				api.ProductDiff{
+					Manifest: api.ManifestDiff{
+						Status: "different",
+						Diff:   "properties:\n+  host: example.com\n-  host: localhost",
+					},
+					RuntimeConfigs: []api.RuntimeConfigsDiff{
+						{
+							Name:   "example-different-runtime-config",
+							Status: "different",
+							Diff:   "addons:\n - name: a-runtime-config\n   jobs:\n   - name: a-job\n     properties:\n+      timeout: 100\n-      timeout: 30",
+						},
+						{
+							Name:   "example-same-runtime-config",
+							Status: "same",
+							Diff:   "",
+						},
+						{
+							Name:   "example-also-different-runtime-config",
+							Status: "different",
+							Diff:   "addons:\n - name: another-runtime-config\n   jobs:\n   - name: another-job\n     properties:\n+      timeout: 110\n-      timeout: 31",
+						},
+					},
+				}, nil)
+		})
 
-		It("succeeds", func(){
+		It("succeeds", func() {
 			diff := commands.NewProductDiff(service, logger)
 			err = diff.Execute([]string{})
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("prints the product manifest diff", func(){
-			// setup
-			service.ProductDiffReturns(
-				api.ProductDiff{
-					Manifest:       api.ManifestDiff{
-						Status: "different",
-						Diff:   "properties:\n+  host: example.com\n-  host: localhost",
-					},
-					RuntimeConfigs: nil,
-			}, nil)
-
-			// execute
+		It("prints the product manifest diff", func() {
 			diff := commands.NewProductDiff(service, logger)
 			err = diff.Execute([]string{"--product", "example-product"})
 			Expect(err).NotTo(HaveOccurred())
@@ -54,12 +69,19 @@ var _ = Describe("ProductDiff", func() {
 			Expect(logBuffer).To(gbytes.Say(regexp.QuoteMeta("properties:\n+  host: example.com\n-  host: localhost\n")))
 		})
 
-		PIt("prints the runtime config diffs", func(){
+		PIt("prints the runtime config diffs", func() {
+			diff := commands.NewProductDiff(service, logger)
+			err = diff.Execute([]string{"--product", "example-product"})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(logBuffer).To(gbytes.Say(regexp.QuoteMeta("Status: different")))
+			Expect(logBuffer).To(gbytes.Say(regexp.QuoteMeta("Status: different")))
+			Expect(logBuffer).To(gbytes.Say(regexp.QuoteMeta("Status: same")))
+			Expect(logBuffer).To(gbytes.Say(regexp.QuoteMeta("Status: different")))
 
 		})
 	})
-	When("there is an error from the diff service", func(){
-		It("returns that error", func(){
+	When("there is an error from the diff service", func() {
+		It("returns that error", func() {
 			// setup
 			service.ProductDiffReturns(
 				api.ProductDiff{}, fmt.Errorf("too many cooks"))
