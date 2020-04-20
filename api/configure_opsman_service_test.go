67f94b62d285952c3709f368988647960d142e15
Kira Boyle <kboyle@pivotal.io>
2020-04-20 15:17:38 -0600

    backfil tests for update ssl certificate
    Signed-off-by: JT Archie <jarchie@pivotal.io>

--- api/configure_opsman_service_test.go
+++ api/configure_opsman_service_test.go
@@ -21,8 +21,8 @@ var _ = Describe("ConfigureOpsmanService", func() {
 		})
 	})
 
-	Describe("ListDeployedProductCredentials", func() {
-		It("lists credential references", func() {
+	Describe("UpdatePivnetToken", func() {
+		It("updates the pivnet token associated with the ops manager", func() {
 			client.AppendHandlers(
 				ghttp.CombineHandlers(
 					ghttp.VerifyRequest("PUT", "/api/v0/settings/pivotal_network_settings"),
@@ -38,7 +38,7 @@ var _ = Describe("ConfigureOpsmanService", func() {
 		})
 
 		When("the api returns an error", func() {
-			It("lists credential references", func() {
+			It("returns the error to the user", func() {
 				client.AppendHandlers(
 					ghttp.CombineHandlers(
 						ghttp.VerifyRequest("PUT", "/api/v0/settings/pivotal_network_settings"),
@@ -47,6 +47,112 @@ var _ = Describe("ConfigureOpsmanService", func() {
 				)
 
 				err := service.UpdatePivnetToken("some-api-token")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("500 Internal Server Error"))
+			})
+		})
+	})
+
+	Describe("UpdateSSLCertificate", func() {
+		It("updates the ssl certificate in ops manager settings", func() {
+			client.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("PUT", "/api/v0/settings/ssl_certificate"),
+					ghttp.RespondWith(http.StatusOK, `{
+					  "ssl_certificate": {
+						"certificate": "some-cert",
+						"private_key": "some-key"
+					  }
+					}`),
+					ghttp.VerifyJSON(`{"certificate": "some-cert","private_key": "some-key"}`),
+				),
+			)
+
+			certInput := api.SSLCertificateInput{
+				CertPem:       "some-cert",
+				PrivateKeyPem: "some-key",
+			}
+			err := service.UpdateSSLCertificate(certInput)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		When("the api returns an error", func() {
+			It("returns the error to the user", func() {
+				client.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("PUT", "/api/v0/settings/ssl_certificate"),
+						ghttp.RespondWith(http.StatusInternalServerError, "{}"),
+					),
+				)
+
+				certInput := api.SSLCertificateInput{
+					CertPem:       "some-cert",
+					PrivateKeyPem: "some-key",
+				}
+				err := service.UpdateSSLCertificate(certInput)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("500 Internal Server Error"))
+			})
+		})
+	})
+
+	Describe("GetSSLCertificate", func() {
+		It("gets the ssl certificate from ops manager settings", func() {
+			client.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("GET", "/api/v0/settings/ssl_certificate"),
+					ghttp.RespondWith(http.StatusOK, `{
+					  "ssl_certificate": {
+					    "certificate": "some-cert"
+					  }
+					}`),
+				),
+			)
+
+			output, err := service.GetSSLCertificate()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(output.Certificate.Certificate).To(Equal("some-cert"))
+		})
+
+		When("the api returns an error", func() {
+			It("returns the error to the user", func() {
+				client.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", "/api/v0/settings/ssl_certificate"),
+						ghttp.RespondWith(http.StatusInternalServerError, "{}"),
+					),
+				)
+
+				_, err := service.GetSSLCertificate()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("500 Internal Server Error"))
+			})
+		})
+	})
+
+	Describe("DeleteSSLCertificate", func() {
+		It("deletes the ssl certificate in ops manager settings", func() {
+			client.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("DELETE", "/api/v0/settings/ssl_certificate"),
+					ghttp.RespondWith(http.StatusOK, `{}`),
+				),
+			)
+
+			err := service.DeleteSSLCertificate()
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		When("the api returns an error", func() {
+			It("returns the error to the user", func() {
+				client.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("DELETE", "/api/v0/settings/ssl_certificate"),
+						ghttp.RespondWith(http.StatusInternalServerError, "{}"),
+					),
+				)
+
+				err := service.DeleteSSLCertificate()
 				Expect(err).To(HaveOccurred())
 				Expect(err.Error()).To(ContainSubstring("500 Internal Server Error"))
 			})
