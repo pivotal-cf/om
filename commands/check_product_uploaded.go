bfcb4155fe3756bb32d57d780f9efb8fafe5451e
JT Archie <jtarchie@gmail.com>
2020-06-23 13:55:00 -0600

    add logging for events in check-product-uploaded
    Signed-off-by: Jesse Alford <jalford@vmware.com>

--- commands/check_product_uploaded.go
+++ commands/check_product_uploaded.go
@@ -9,7 +9,8 @@ import (
 )
 
 type CheckProductUploaded struct {
-	service   availableProductsService
+	service availableProductsService
+	logger  logger
 
 	Options struct {
 		interpolateConfigFileOptions
@@ -32,6 +33,8 @@ func (c CheckProductUploaded) Execute(args []string) error {
 		options.FileGlob,
 		options.PivnetDisableSSL,
 	)
+
+	c.logger.Println("Loading product metadata from Pivnet...")
 	contents, err := provider.MetadataBytes()
 	if err != nil {
 		return fmt.Errorf("could not parse response from Pivnet: %s", err)
@@ -44,6 +47,7 @@ func (c CheckProductUploaded) Execute(args []string) error {
 
 	name, version := metadata.ProductName(), metadata.ProductVersion()
 
+	c.logger.Println("Loading all available products from OpsManager...")
 	output, err := c.service.ListAvailableProducts()
 	if err != nil {
 		return fmt.Errorf("could not find products on OpsManager: %s", err)
@@ -51,19 +55,21 @@ func (c CheckProductUploaded) Execute(args []string) error {
 
 	for _, productInfo := range output.ProductsList {
 		if productInfo.Name == name && productInfo.Version == version {
+			c.logger.Println(fmt.Sprintf("Product %q of version %q found on OpsManager", name, version))
 			return nil
 		}
 	}
 
-	return fmt.Errorf("product %s of version %s could not be found on OpsManager", name, version)
+	return fmt.Errorf("product %q of version %q could not be found on OpsManager", name, version)
 }
 
 func (c CheckProductUploaded) Usage() jhanda.Usage {
 	return jhanda.Usage{}
 }
 
-func NewCheckProductUploaded(apService availableProductsService) *CheckProductUploaded {
+func NewCheckProductUploaded(apService availableProductsService, logger logger) *CheckProductUploaded {
 	return &CheckProductUploaded{
 		service: apService,
+		logger:  logger,
 	}
 }
