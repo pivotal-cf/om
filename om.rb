8d4aaa76b3db91fd523604aa2a589a4f64d210ee
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-22 15:16:00 -0600

    Brew formula update for om version 6.0.1

--- om.rb
+++ om.rb
@@ -2,16 +2,16 @@
 class Om < Formula
   desc ""
   homepage ""
-  version "6.0.0"
+  version "6.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pivotal-cf/om/releases/download/6.0.0/om-darwin-6.0.0.tar.gz"
-    sha256 "7f2c3f0ba692d1a52f41b7ffc98fbd655386bcc393963ed4eef53310e2c33913"
+    url "https://github.com/pivotal-cf/om/releases/download/6.0.1/om-darwin-6.0.1.tar.gz"
+    sha256 "5cad51dc937f192b9ca6d6e25426c6ea2de2f70d714dda0183ee05071c1320c0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/pivotal-cf/om/releases/download/6.0.0/om-linux-6.0.0.tar.gz"
-      sha256 "52edce7bbc13295abc76790d68f6795417d6cefc064750f0a215b23acb3e68e2"
+      url "https://github.com/pivotal-cf/om/releases/download/6.0.1/om-linux-6.0.1.tar.gz"
+      sha256 "77f92a2feca1f9c04c0a2f8c39b5d446bd3e3381481047b7434eed930928d3f8"
     end
   end
 
