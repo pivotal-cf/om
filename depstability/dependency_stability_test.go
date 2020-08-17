437510ed22ec2e3f4ffa253b1588c33ccfd65f11
JT Archie <jarchie@vmware.com>
2020-08-17 14:07:53 -0700

    Test dependency stability [#173909198]
    Signed-off-by: Jesse Alford <jalford@vmware.com>
    Co-authored-by: Jesse Alford <jalford@vmware.com>

---
+++ depstability/dependency_stability_test.go
@@ -0,0 +1,47 @@
+package depstability_test
+
+import (
+	"io/ioutil"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Dependency Topology", func() {
+	It("is unchanged, allowing reuse of the 4.3.0 OSL file", func() {
+		currentDepnames, err := listDepnamesFromGoSum()
+		Expect(err).NotTo(HaveOccurred())
+		oldDepnames, err := listDepnamesFromRecords()
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(currentDepnames).To(ConsistOf(oldDepnames), "See readme for guidance on updating fixtures")
+	})
+})
+
+func listDepnamesFromGoSum() (deplist []string, err error) {
+	gosum, err := ioutil.ReadFile("../go.sum")
+	if err != nil {
+		return
+	}
+	trimmedGoSum := strings.TrimSpace(string(gosum))
+	splitGoSum := strings.Split(trimmedGoSum, "\n")
+	var deplistWithDuplication []string
+	for _, line := range splitGoSum {
+		splitLine := strings.Split(line, " ")
+		deplistWithDuplication = append(deplistWithDuplication, splitLine[0])
+	}
+	for _, line := range deplistWithDuplication {
+		if len(deplist) == 0 || deplist[len(deplist)-1] != line {
+			deplist = append(deplist, line)
+		}
+	}
+	return
+}
+
+func listDepnamesFromRecords() (deplist []string, err error) {
+	depRecords, err := ioutil.ReadFile("records/depnames-6.1.2.txt")
+	trimmedDepRecords := strings.TrimSpace(string(depRecords))
+	deplist = strings.Split(trimmedDepRecords, "\n")
+	return
+}
