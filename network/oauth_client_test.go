af5a64669f02be8c468d82ca8cc5bc75d4df208b
JT Archie <jtarchie@gmail.com>
2020-12-02 15:19:41 -0700

    fix tests to be ghttp

--- network/oauth_client_test.go
+++ network/oauth_client_test.go
@@ -1,15 +1,13 @@
 package network_test
 
 import (
-	"bufio"
-	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"github.com/onsi/gomega/ghttp"
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"net/http/httputil"
 	"net/url"
 	"strings"
 
@@ -23,50 +21,36 @@ import (
 
 var _ = Describe("OAuthClient", func() {
 	var (
-		receivedRequest []byte
-		authHeader      string
-		callCount       int
-		server          *httptest.Server
+		server     *ghttp.Server
 	)
 
 	BeforeEach(func() {
-		callCount = 0
-		server = httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			switch req.URL.Path {
-			case "/uaa/oauth/token":
-				callCount++
-				var err error
-				receivedRequest, err = httputil.DumpRequest(req, true)
-				Expect(err).ToNot(HaveOccurred())
-
-				w.Header().Set("Content-Type", "application/json")
-
-				_, err = w.Write([]byte(`{
-					"access_token": "some-opsman-token",
-					"token_type": "bearer",
-					"expires_in": 3600
-					}`))
-				Expect(err).ToNot(HaveOccurred())
-			case "/some/path":
-				authHeader = req.Header.Get("Authorization")
-
-				http.SetCookie(w, &http.Cookie{
-					Name:  "somecookie",
-					Value: "somevalue",
-				})
-
-				w.WriteHeader(http.StatusNoContent)
-			}
-		}))
-		server.Config.ErrorLog = log.New(GinkgoWriter, "", 0)
+		server = ghttp.NewTLSServer()
 	})
 
 	Describe("Do", func() {
 		It("makes a request with authentication", func() {
-			client, err := network.NewOAuthClient(server.URL, "opsman-username", "opsman-password", "", "", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
-			Expect(err).ToNot(HaveOccurred())
+			server.RouteToHandler("POST", "/uaa/oauth/token", ghttp.CombineHandlers(
+				ghttp.VerifyBasicAuth("opsman", ""),
+				ghttp.VerifyForm(url.Values{
+					"grant_type":   []string{"password"},
+					"username":     []string{"opsman-username"},
+					"password":     []string{"opsman-password"},
+					"token_format": []string{"opaque"},
+					"client_id":    []string{"opsman"},
+				}),
+				ghttp.RespondWith(http.StatusOK, `{
+					"access_token": "some-opsman-token",
+					"token_type": "bearer",
+					"expires_in": 3600
+					}`, http.Header{
+					"Content-Type": []string{"application/json"},
+				}),
+			))
+			server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
 
-			Expect(callCount).To(Equal(0))
+			client, err := network.NewOAuthClient(server.URL(), "opsman-username", "opsman-password", "", "", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
+			Expect(err).ToNot(HaveOccurred())
 
 			req, err := http.NewRequest("GET", "/some/path", strings.NewReader("request-body"))
 			Expect(err).ToNot(HaveOccurred())
@@ -74,36 +58,28 @@ var _ = Describe("OAuthClient", func() {
 			resp, err := client.Do(req)
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(resp.StatusCode).To(Equal(http.StatusNoContent))
-
-			Expect(authHeader).To(Equal("Bearer some-opsman-token"))
-
-			req, err = http.ReadRequest(bufio.NewReader(bytes.NewReader(receivedRequest)))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(req.Method).To(Equal("POST"))
-			Expect(req.URL.Path).To(Equal("/uaa/oauth/token"))
-
-			username, password, ok := req.BasicAuth()
-			Expect(ok).To(BeTrue())
-			Expect(username).To(Equal("opsman"))
-			Expect(password).To(BeEmpty())
-
-			err = req.ParseForm()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(req.Form).To(Equal(url.Values{
-				"grant_type":   []string{"password"},
-				"username":     []string{"opsman-username"},
-				"password":     []string{"opsman-password"},
-				"token_format": []string{"opaque"},
-				"client_id":    []string{"opsman"},
-			}))
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
 		})
 
 		It("makes a request with client credentials", func() {
-			client, err := network.NewOAuthClient(server.URL, "", "", "client_id", "client_secret", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
-			Expect(err).ToNot(HaveOccurred())
+			server.RouteToHandler("POST", "/uaa/oauth/token", ghttp.CombineHandlers(
+				ghttp.VerifyBasicAuth("client_id", "client_secret"),
+				ghttp.VerifyForm(url.Values{
+					"grant_type":   []string{"client_credentials"},
+					"token_format": []string{"opaque"},
+				}),
+				ghttp.RespondWith(http.StatusOK, `{
+					"access_token": "some-opsman-token",
+					"token_type": "bearer",
+					"expires_in": 3600
+					}`, http.Header{
+					"Content-Type": []string{"application/json"},
+				}),
+			))
+			server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
 
-			Expect(callCount).To(Equal(0))
+			client, err := network.NewOAuthClient(server.URL(), "", "", "client_id", "client_secret", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
+			Expect(err).ToNot(HaveOccurred())
 
 			req, err := http.NewRequest("GET", "/some/path", strings.NewReader("request-body"))
 			Expect(err).ToNot(HaveOccurred())
@@ -111,26 +87,7 @@ var _ = Describe("OAuthClient", func() {
 			resp, err := client.Do(req)
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(resp.StatusCode).To(Equal(http.StatusNoContent))
-
-			Expect(authHeader).To(Equal("Bearer some-opsman-token"))
-
-			req, err = http.ReadRequest(bufio.NewReader(bytes.NewReader(receivedRequest)))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(req.Method).To(Equal("POST"))
-			Expect(req.URL.Path).To(Equal("/uaa/oauth/token"))
-
-			username, password, ok := req.BasicAuth()
-			Expect(ok).To(BeTrue())
-			Expect(username).To(Equal("client_id"))
-			Expect(password).To(Equal("client_secret"))
-
-			err = req.ParseForm()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(req.Form).To(Equal(url.Values{
-				"grant_type":   []string{"client_credentials"},
-				"token_format": []string{"opaque"},
-			}))
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
 		})
 
 		It("enforces minimum TLS version 1.2", func() {
@@ -151,7 +108,9 @@ var _ = Describe("OAuthClient", func() {
 
 		When("passing a url with no scheme", func() {
 			It("defaults to HTTPS", func() {
-				noScheme, err := url.Parse(server.URL)
+				setupBasicOauth(server)
+
+				noScheme, err := url.Parse(server.URL())
 				Expect(err).ToNot(HaveOccurred())
 
 				noScheme.Scheme = ""
@@ -166,14 +125,14 @@ var _ = Describe("OAuthClient", func() {
 				resp, err := client.Do(req)
 				Expect(err).ToNot(HaveOccurred())
 
-				Expect(resp.StatusCode).To(Equal(http.StatusNoContent))
+				Expect(resp.StatusCode).To(Equal(http.StatusOK))
 			})
 		})
 
 		When("insecureSkipVerify is configured", func() {
 			When("it is set to false", func() {
 				It("throws an error for invalid certificates", func() {
-					client, err := network.NewOAuthClient(server.URL, "opsman-username", "opsman-password", "", "", false, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
+					client, err := network.NewOAuthClient(server.URL(), "opsman-username", "opsman-password", "", "", false, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
 					Expect(err).ToNot(HaveOccurred())
 
 					req, err := http.NewRequest("GET", "/some/path", strings.NewReader("request-body"))
@@ -186,7 +145,9 @@ var _ = Describe("OAuthClient", func() {
 
 			When("it is set to true", func() {
 				It("does not verify certificates", func() {
-					client, err := network.NewOAuthClient(server.URL, "opsman-username", "opsman-password", "", "", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
+					setupBasicOauth(server)
+
+					client, err := network.NewOAuthClient(server.URL(), "opsman-username", "opsman-password", "", "", true, "", time.Duration(5)*time.Second, time.Duration(30)*time.Second)
 					Expect(err).ToNot(HaveOccurred())
 
 					req, err := http.NewRequest("GET", "/some/path", strings.NewReader("request-body"))
@@ -200,12 +161,14 @@ var _ = Describe("OAuthClient", func() {
 
 		When("supporting a ca cert", func() {
 			It("loads from a string", func() {
-				cert, err := x509.ParseCertificate(server.TLS.Certificates[0].Certificate[0])
+				setupBasicOauth(server)
+
+				cert, err := x509.ParseCertificate(server.HTTPTestServer.TLS.Certificates[0].Certificate[0])
 				Expect(err).ToNot(HaveOccurred())
 				pemCert := string(pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}))
 
 				client, err := network.NewOAuthClient(
-					server.URL,
+					server.URL(),
 					"opsman-username", "opsman-password",
 					"", "",
 					false,
@@ -223,12 +186,14 @@ var _ = Describe("OAuthClient", func() {
 			})
 
 			It("loads from a file", func() {
-				cert, err := x509.ParseCertificate(server.TLS.Certificates[0].Certificate[0])
+				setupBasicOauth(server)
+
+				cert, err := x509.ParseCertificate(server.HTTPTestServer.TLS.Certificates[0].Certificate[0])
 				Expect(err).ToNot(HaveOccurred())
 				pemCert := writeFile(string(pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})))
 
 				client, err := network.NewOAuthClient(
-					server.URL,
+					server.URL(),
 					"opsman-username", "opsman-password",
 					"", "",
 					false,
@@ -284,3 +249,24 @@ var _ = Describe("OAuthClient", func() {
 		})
 	})
 })
+
+func setupBasicOauth(server *ghttp.Server) {
+	server.RouteToHandler("POST", "/uaa/oauth/token", ghttp.CombineHandlers(
+		ghttp.VerifyBasicAuth("opsman", ""),
+		ghttp.VerifyForm(url.Values{
+			"grant_type":   []string{"password"},
+			"username":     []string{"opsman-username"},
+			"password":     []string{"opsman-password"},
+			"token_format": []string{"opaque"},
+			"client_id":    []string{"opsman"},
+		}),
+		ghttp.RespondWith(http.StatusOK, `{
+					"access_token": "some-opsman-token",
+					"token_type": "bearer",
+					"expires_in": 3600
+					}`, http.Header{
+			"Content-Type": []string{"application/json"},
+		}),
+	))
+	server.AppendHandlers(ghttp.RespondWith(http.StatusOK, nil))
+}
