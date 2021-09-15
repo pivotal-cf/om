4656f7e67d06d37fbcb797197c4a496b511007b1
Wayne Adams <wadams@pivotal.io>
2021-09-15 13:12:15 -0700

    Update test to account for variable ContentLength
    Remove unused package

--- formcontent/formcontent_test.go
+++ formcontent/formcontent_test.go
@@ -191,7 +191,7 @@ var _ = Describe("Formcontent", func() {
 			content, err := ioutil.ReadAll(submission.Content)
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(submission.ContentLength).To(Equal(int64(373)))
+			Expect(submission.ContentLength).To(Or(Equal(int64(373)), Equal(int64(374))))
 			Expect(string(content)).To(MatchRegexp(`^--\w+\r\nContent-Disposition: form-data; name=\"file1\"; filename=\"\w+\"\r\n` +
 				`Content-Type: application/octet-stream\r\n\r\n` +
 				`some content` +
