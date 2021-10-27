282dcfd14d62467c6615c7b85f0031b35d9fee7a
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-27 08:38:57 -0600

    Brew formula update for om version 7.4.0

--- om.rb
+++ om.rb
@@ -5,25 +5,28 @@
 class Om < Formula
   desc ""
   homepage ""
-  version "7.3.2"
-  bottle :unneeded
+  version "7.4.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pivotal-cf/om/releases/download/7.3.2/om-darwin-7.3.2.tar.gz"
-      sha256 "7a78819b4698c4a5337de5d3c77730c23cebdc7c7dd502f93679914ab9c6b08a"
+      url "https://github.com/pivotal-cf/om/releases/download/7.4.0/om-darwin-7.4.0.tar.gz"
+      sha256 "606dabdfb3793ec2392ea72cf39d20f30d9db6eac9dbefbcbbcca38c83e90dc9"
+
+      def install
+        bin.install "om"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pivotal-cf/om/releases/download/7.3.2/om-linux-7.3.2.tar.gz"
-      sha256 "352965ec4d8be070e0e00baf4fec77eaaf20c6590acb00740c2d378ecb44dfb9"
-    end
-  end
+      url "https://github.com/pivotal-cf/om/releases/download/7.4.0/om-linux-7.4.0.tar.gz"
+      sha256 "67ebe55fd206217e551891c81121ec60c1aa593a6a8723485c0b6ba4ce17a06a"
 
-  def install
-    bin.install "om"
+      def install
+        bin.install "om"
+      end
+    end
   end
 
   test do
