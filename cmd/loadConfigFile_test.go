2362ae93f58d8c7a4e69bd79ee61f77b124a2891
Jhonathan Aristizabal <jhonathana@vmware.com>
2022-01-07 08:37:52 -0500

    Removed double quotes escaping
    Removed escaping of double-quotes on parameter values which caused
    problems when parsing correctly formed parameters.
    
    Authored-by: Jhonathan Aristizabal <jhonathana@vmware.com>

--- cmd/loadConfigFile_test.go
+++ cmd/loadConfigFile_test.go
@@ -11,11 +11,11 @@ var fakeOptions map[string]interface{}
 
 var _ = Describe("parseOptions", func() {
 	BeforeEach(func() {
-		fakeOptions = map[string]interface{}{"test-name": "\"pas"}
+		fakeOptions = map[string]interface{}{"test-name": `"pas`}
 	})
 
-	It("Should parse values with double quotes", func() {
-		expected := []string{"--test-name=\\\"pas"}
+	It("Should parse values with double quotes not escaping it", func() {
+		expected := []string{`--test-name="pas`}
 		val, err := parseOptions(fakeOptions)
 		Expect(err).To(BeNil())
 		Expect(val).To(Equal(expected))
