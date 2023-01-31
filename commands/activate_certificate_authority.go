b436fc3c4e17d8284d7a6b81a4ac50b845bf3422
David Timm <dtimm@vmware.com>
2023-01-31 10:18:10 -0700

    feat: allow no guid activate ca workflow
    - this will activate any new inactive CA
    
    Co-authored-by: Ramkumar Vengadakrishnan <rvengadakrishnan@pivotal.io@vmware.com>

--- commands/activate_certificate_authority.go
+++ commands/activate_certificate_authority.go
@@ -3,6 +3,9 @@ package commands
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/pivotal-cf/om/api"
 )
 
@@ -10,13 +13,14 @@ type ActivateCertificateAuthority struct {
 	service activateCertificateAuthorityService
 	logger  logger
 	Options struct {
-		Id string `long:"id" required:"true" description:"certificate authority id"`
+		Id string `long:"id" required:"false" description:"certificate authority id"`
 	}
 }
 
 //counterfeiter:generate -o ./fakes/activate_certificate_authority_service.go --fake-name ActivateCertificateAuthorityService . activateCertificateAuthorityService
 type activateCertificateAuthorityService interface {
 	ActivateCertificateAuthority(api.ActivateCertificateAuthorityInput) error
+	ListCertificateAuthorities() (api.CertificateAuthoritiesOutput, error)
 }
 
 func NewActivateCertificateAuthority(service activateCertificateAuthorityService, logger logger) *ActivateCertificateAuthority {
@@ -24,8 +28,39 @@ func NewActivateCertificateAuthority(service activateCertificateAuthorityService
 }
 
 func (a ActivateCertificateAuthority) Execute(args []string) error {
+	guid := a.Options.Id
+	if a.Options.Id == "" {
+		caList, _ := a.service.ListCertificateAuthorities()
+		var activeCA api.CA
+		var inactiveCA api.CA
+
+		for _, ca := range caList.CAs {
+			if ca.Active {
+				activeCA = ca
+			} else {
+				inactiveCA = ca
+			}
+		}
+
+		if inactiveCA.GUID == "" {
+			return fmt.Errorf("no inactive certificate authorities to activate")
+		}
+
+		inactiveCreationTime, _ := time.Parse(time.RFC3339, inactiveCA.CreatedOn)
+		fmt.Printf("%+v\n", inactiveCreationTime)
+		activeCreationTime, _ := time.Parse(time.RFC3339, activeCA.CreatedOn)
+		fmt.Printf("%+v\n", activeCreationTime)
+
+		if activeCreationTime.After(inactiveCreationTime) {
+			a.logger.Printf("No newer certificate authority available to activate\n")
+			return nil
+		}
+
+		guid = inactiveCA.GUID
+	}
+
 	err := a.service.ActivateCertificateAuthority(api.ActivateCertificateAuthorityInput{
-		GUID: a.Options.Id,
+		GUID: guid,
 	})
 
 	if err != nil {
--- commands/activate_certificate_authority_test.go
+++ commands/activate_certificate_authority_test.go
@@ -41,6 +41,86 @@ var _ = Describe("ActivateCertificateAuthority", func() {
 			Expect(fmt.Sprintf(format, content...)).To(Equal("Certificate authority 'some-certificate-authority-id' activated\n"))
 		})
 
+		Context("with no guid specified", func() {
+			args := []string{}
+			Context("with an inactive CA newer than the active CA", func() {
+				BeforeEach(func() {
+					fakeService.ListCertificateAuthoritiesReturns(api.CertificateAuthoritiesOutput{CAs: []api.CA{
+						{
+							GUID:      "active-ca-guid",
+							Active:    true,
+							CreatedOn: "2015-06-16T05:17:43Z",
+						},
+						{
+							GUID:      "inactive-ca-guid",
+							Active:    false,
+							CreatedOn: "2025-06-16T05:17:44Z",
+						},
+					}}, nil)
+				})
+
+				It("activates the inactive CA", func() {
+					err := executeCommand(command, args)
+					Expect(err).ToNot(HaveOccurred())
+
+					Expect(fakeService.ListCertificateAuthoritiesCallCount()).To(Equal(1), "list certificates call count")
+					Expect(fakeService.ActivateCertificateAuthorityCallCount()).To(Equal(1), "activate certificate call count")
+					Expect(fakeService.ActivateCertificateAuthorityArgsForCall(0)).To(Equal(api.ActivateCertificateAuthorityInput{
+						GUID: "inactive-ca-guid",
+					}), "activate ca API args")
+				})
+			})
+
+			Context("with an inactive CA older than the active CA", func() {
+				BeforeEach(func() {
+					fakeService.ListCertificateAuthoritiesReturns(api.CertificateAuthoritiesOutput{CAs: []api.CA{
+						{
+							GUID:      "active-ca-guid",
+							Active:    true,
+							CreatedOn: "1995-06-16T05:17:43Z",
+						},
+						{
+							GUID:      "inactive-ca-guid",
+							Active:    false,
+							CreatedOn: "1895-06-16T05:17:44Z",
+						},
+					}}, nil)
+				})
+
+				It("makes no activate call", func() {
+					err := executeCommand(command, args)
+					Expect(err).ToNot(HaveOccurred())
+
+					Expect(fakeService.ListCertificateAuthoritiesCallCount()).To(Equal(1), "list certificates call count")
+					Expect(fakeService.ActivateCertificateAuthorityCallCount()).To(Equal(0), "activate certificate call count")
+
+					Expect(fakeLogger.PrintfCallCount()).To(Equal(1))
+					format, content := fakeLogger.PrintfArgsForCall(0)
+					Expect(fmt.Sprintf(format, content...)).To(Equal("No newer certificate authority available to activate\n"))
+				})
+			})
+
+			Context("with no inactive CA", func() {
+				BeforeEach(func() {
+					fakeService.ListCertificateAuthoritiesReturns(api.CertificateAuthoritiesOutput{CAs: []api.CA{
+						{
+							GUID:      "active-ca-guid",
+							Active:    true,
+							CreatedOn: "2023-01-31T12:00:00Z",
+						},
+					}}, nil)
+				})
+
+				It("returns an error", func() {
+					err := executeCommand(command, args)
+					Expect(err).To(MatchError("no inactive certificate authorities to activate"))
+
+					Expect(fakeService.ListCertificateAuthoritiesCallCount()).To(Equal(1), "list certificates call count")
+					Expect(fakeService.ActivateCertificateAuthorityCallCount()).To(Equal(0), "activate certificate call count")
+				})
+			})
+		})
+
 		When("the service fails to activate a certificate", func() {
 			It("returns an error", func() {
 				fakeService.ActivateCertificateAuthorityReturns(errors.New("failed to activate certificate"))
