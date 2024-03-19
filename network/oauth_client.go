70d4a5ffd561a3842e0b0d67ec5da775133cc8a5
Janice Bailey <janice.bailey@broadcom.com>
2024-03-19 09:58:32 -0400

    created a new oauth_client.go file to include DNS cachedResolver

--- network/oauth_client.go
+++ network/oauth_client.go
@@ -1,15 +1,61 @@
 package network
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/cloudfoundry-community/go-uaa"
 	"golang.org/x/oauth2"
 )
+
+// CachedResolver caches DNS lookups
+type CachedResolver struct {
+	cache      map[string]string
+	cacheMutex sync.Mutex
+	ttl        time.Duration
+}
+
+func NewCachedResolver(ttl time.Duration) *CachedResolver {
+	return &CachedResolver{
+		cache: make(map[string]string),
+		ttl:   ttl,
+	}
+}
+
+func (r *CachedResolver) Resolve(host string) (string, error) {
+	r.cacheMutex.Lock()
+	defer r.cacheMutex.Unlock()
+
+	if ip, found := r.cache[host]; found {
+		return ip, nil
+	}
+
+	ips, err := net.LookupHost(host)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ips) == 0 {
+		return "", fmt.Errorf("no IPs found for host: %s", host)
+	}
+
+	r.cache[host] = ips[0]
+
+	go func() {
+		time.Sleep(r.ttl)
+		r.cacheMutex.Lock()
+		delete(r.cache, host)
+		r.cacheMutex.Unlock()
+	}()
+
+	return ips[0], nil
+}
 
 type OAuthClient struct {
 	caCert             string
@@ -22,6 +68,7 @@ type OAuthClient struct {
 	username           string
 	connectTimeout     time.Duration
 	requestTimeout     time.Duration
+	resolver           *CachedResolver // Added resolver
 }
 
 func NewOAuthClient(
@@ -29,9 +76,9 @@ func NewOAuthClient(
 	clientID, clientSecret string,
 	insecureSkipVerify bool,
 	caCert string,
-	connectTimeout time.Duration,
-	requestTimeout time.Duration,
+	connectTimeout, requestTimeout time.Duration,
 ) (*OAuthClient, error) {
+	resolver := NewCachedResolver(5 * time.Minute) // Example TTL
 	return &OAuthClient{
 		caCert:             caCert,
 		clientID:           clientID,
@@ -42,7 +89,20 @@ func NewOAuthClient(
 		username:           username,
 		connectTimeout:     connectTimeout,
 		requestTimeout:     requestTimeout,
+		resolver:           resolver, // Initialize resolver
 	}, nil
+}
+
+func (oc *OAuthClient) customDialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	separator := strings.LastIndex(addr, ":")
+	host, port := addr[:separator], addr[separator+1:]
+
+	resolvedIP, err := oc.resolver.Resolve(host)
+	if err != nil {
+		return nil, err
+	}
+
+	return net.Dial(network, resolvedIP+":"+port)
 }
 
 func (oc *OAuthClient) Do(request *http.Request) (*http.Response, error) {
@@ -55,7 +115,7 @@ func (oc *OAuthClient) Do(request *http.Request) (*http.Response, error) {
 
 	targetURL, err := url.Parse(target)
 	if err != nil {
-		return nil, fmt.Errorf("could not parse target url: %s", err)
+		return nil, fmt.Errorf("could not parse target URL: %w", err)
 	}
 
 	targetURL.Path = "/uaa"
@@ -63,22 +123,15 @@ func (oc *OAuthClient) Do(request *http.Request) (*http.Response, error) {
 	request.URL.Scheme = targetURL.Scheme
 	request.URL.Host = targetURL.Host
 
-	client, err := newHTTPClient(
-		oc.insecureSkipVerify,
-		oc.caCert,
-		oc.requestTimeout,
-		oc.connectTimeout,
-	)
-
-	if err != nil {
-		return nil, err
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: oc.customDialContext,
+		},
+		Timeout: oc.requestTimeout,
 	}
 
 	if token != nil && token.Valid() {
-		request.Header.Set(
-			"Authorization",
-			fmt.Sprintf("Bearer %s", token.AccessToken),
-		)
+		request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
 		return client.Do(request)
 	}
 
@@ -90,26 +143,12 @@ func (oc *OAuthClient) Do(request *http.Request) (*http.Response, error) {
 	var authOption uaa.AuthenticationOption
 
 	if oc.username != "" && oc.password != "" {
-		authOption = uaa.WithPasswordCredentials(
-			"opsman",
-			"",
-			oc.username,
-			oc.password,
-			uaa.JSONWebToken,
-		)
+		authOption = uaa.WithPasswordCredentials("opsman", "", oc.username, oc.password, uaa.JSONWebToken)
 	} else {
-		authOption = uaa.WithClientCredentials(
-			oc.clientID,
-			oc.clientSecret,
-			uaa.JSONWebToken,
-		)
+		authOption = uaa.WithClientCredentials(oc.clientID, oc.clientSecret, uaa.JSONWebToken)
 	}
 
-	api, err := uaa.New(
-		targetURL.String(),
-		authOption,
-		options...,
-	)
+	api, err := uaa.New(targetURL.String(), authOption, options...)
 	if err != nil {
 		return nil, fmt.Errorf("could not init UAA client: %w", err)
 	}
@@ -122,14 +161,10 @@ func (oc *OAuthClient) Do(request *http.Request) (*http.Response, error) {
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("token could not be retrieved from target url: %w", err)
+		return nil, fmt.Errorf("token could not be retrieved from target URL: %w", err)
 	}
 
-	request.Header.Set(
-		"Authorization",
-		fmt.Sprintf("Bearer %s", token.AccessToken),
-	)
-
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
 	oc.token = token
 
 	return client.Do(request)
