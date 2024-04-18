42b4b77c0266dda544928cbcea7980cd9df99c98
Steve Taylor <sttaylor@vmware.com>
2024-04-18 16:17:10 -0700

    fix: Ensure image has go 1.22.2
    I couldn't figure out an easier way to update the goreleaser/goreleaser
    image.  If I had my 'druthers, I'd have created a new image "from"
    goreleaser and add a step to update go.  Making the task do this work
    seems the most expedient, but it's a kludge, so you'll want to revert
    this in favor of a better solution in the future.
    
    Co-authored-by: Steve Taylor <sttaylor@vmware.com>

--- ci/tasks/build/task.sh
+++ ci/tasks/build/task.sh
@@ -11,6 +11,12 @@ cd om
 git remote set-url origin https://github.com/pivotal-cf/om
 git fetch -t -P -p
 
+# Kludge to get this thing buillt by go 1.22.2
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+tar xf go1.22.2.linux-amd64.tar.gz
+rm -rf /usr/local/go
+mv go /usr/local
+
 go version
 goreleaser release
 
