efdc352be8e95ada216a07bc91620db1aaf8774d
Steve Taylor <sttaylor@vmware.com>
2024-04-18 18:38:25 -0700

    fix: delete kludge tarball so we can commit
    Downloading the golang tarball into the git repo causes an issue with
    committing the version file update. This fix removes the file so that we
    can check in and move on.
    
    Co-authored-by: Steve Taylor <sttaylor@vmware.com>

--- ci/tasks/build/task.sh
+++ ci/tasks/build/task.sh
@@ -14,6 +14,7 @@ git fetch -t -P -p
 # Kludge to get this thing buillt by go 1.22.2
 wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
 tar xf go1.22.2.linux-amd64.tar.gz
+rm go1.22.2.linux-amd64.tar.gz
 rm -rf /usr/local/go
 mv go /usr/local
 
